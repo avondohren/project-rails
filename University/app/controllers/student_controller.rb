53ac6962a3e3f65bb17955af1209d633fdbba81e
matthova <matthova87@gmail.com>
2014-03-19 16:00:49 -0500

    Added REST style to student controller

--- University/app/controllers/student_controller.rb
+++ University/app/controllers/student_controller.rb
@@ -1,9 +1,54 @@
 class StudentController < ActionController::Base
-  def list
+  def index
     @students = Student.all
+  end
+  
+  def show
+    @student = Student.find(params[:id])
+  end
+  
+  def new
+    # NO VARIABLE CALLED @article :(
+  end
+  
+  def create
+    @student = Student.new({:fname => params[:fname], :lname =< params[:lname]})
+    
+    if @student.save # If this fails to save, it returns FALSE.
+      
+      # For successful saves:
+      redirect_to(:students)
+      
+    else
+      # Show the errors to the user.
+      render "new"
+    end
+  end
+  
+  def edit
+    @student = Student.find(params[:id])
+  end
+  
+  def update
+    @student = Student.find(params[:id])
+    
+    if @student.update_attributes({:fname => params[:fname], :lname =< params[:lname]})
+      redirect_to(student_path(@student.id))
+    else
+      render "new"
+    end
+  end
+  
+  def delete
+    @student = Studnet.find(params[:id])
+    
+    @student.delete
+    
+    redirect_to(:students)
   end
   
   def name 
     fname + " " + lname
   end
+
 end
--- University/app/models/student.rb
+++ University/app/models/student.rb
@@ -2,4 +2,7 @@ class Student < ActiveRecord::Base
   attr_accessible :fname, :lname
   
   has_and_belongs_to_many :klasses
+  
+  validates :fname, :presence => true
+  validates :lname, :presence => true
 end
