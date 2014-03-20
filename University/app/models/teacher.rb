6f5166d742d86ac502644bdfe9cb3e665d17da02
matthova <matthova87@gmail.com>
2014-03-20 09:45:28 -0500

    Fixed teacher db has mas klasses

--- University/app/models/teacher.rb
+++ University/app/models/teacher.rb
@@ -1,7 +1,7 @@
 class Teacher < ActiveRecord::Base
   attr_accessible :fname, :lname
   
-  has_and_belongs_to_many :klasses
+  has_many :klasses
   
   validates :fname, :presence => true
   validates :lname, :presence => true
